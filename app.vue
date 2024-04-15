e06b4e94056aa22729230dd6c99271a492184b13
hidehyshi <hideyuki.hayashi@askul.com>
2024-04-15 19:55:58 +0900

    first commit

---
+++ app.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+useHead({
+  title: "web-front", // titleタグ設定
+  link: [
+    // GoogleFonts設定
+    {
+      rel: "stylesheet",
+      href: "https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&display=swap",
+    },
+  ],
+});
+</script>
+<template>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
